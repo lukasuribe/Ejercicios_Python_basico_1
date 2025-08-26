9ff9ca776802f9aedf645e656e585e5dfb56ce4d
lukasuribe <luribe@ucm.es>
2025-08-26 18:51:29 +0200

    1-7
    Ejercicios terminados

---
+++ ejercicio5.py
@@ -0,0 +1,90 @@
+lista_campañas = [
+    {
+        'nombre': 'Rebajas de Enero',
+        'presupuesto': 35000,
+        'inicio': '2023-01-02',
+        'fin': '2023-01-31',
+        'medios': ['Publicidad en línea', 'Redes Sociales', 'Email Marketing'],
+        'segmentos_objetivo': ['Clientes regulares', 'Potenciales compradores'],
+        'personas_alcanzadas': 50000,
+        'tasa_conversion': 0.02
+    },
+    {
+        'nombre': 'San Valentín',
+        'presupuesto': 25000,
+        'inicio': '2023-02-01',
+        'fin': '2023-02-14',
+        'medios': ['Redes Sociales', 'Email Marketing', 'Publicidad impresa'],
+        'segmentos_objetivo': ['Parejas', 'Jóvenes'],
+        'personas_alcanzadas': 30000,
+        'tasa_conversion': 0.05
+    },
+    {
+        'nombre': 'Día del Padre',
+        'presupuesto': 20000,
+        'inicio': '2023-06-01',
+        'fin': '2023-06-15',
+        'medios': ['Redes Sociales', 'Colaboraciones con influencers', 'Publicidad impresa'],
+        'segmentos_objetivo': ['Jóvenes', 'Familias'],
+        'personas_alcanzadas': 25000,
+        'tasa_conversion': 0.03
+    },
+    {
+        'nombre': 'Día de la Madre',
+        'presupuesto': 22000,
+        'inicio': '2023-04-20',
+        'fin': '2023-05-10',
+        'medios': ['Publicidad en línea', 'Redes Sociales', 'Email Marketing'],
+        'segmentos_objetivo': ['Jóvenes', 'Familias'],
+        'personas_alcanzadas': 28000,
+        'tasa_conversion': 0.04
+    },
+    {
+        'nombre': 'Día del Friki',
+        'presupuesto': 18000,
+        'inicio': '2023-05-20',
+        'fin': '2023-05-25',
+        'medios': ['Publicidad en tiendas físicas', 'Redes Sociales', 'Colaboraciones con influencers'],
+        'segmentos_objetivo': ['Jóvenes'],
+        'personas_alcanzadas': 20000,
+        'tasa_conversion': 0.05
+    },
+    {
+        'nombre': 'Black Friday',
+        'presupuesto': 50000,
+        'inicio': '2023-11-21',
+        'fin': '2023-11-30',
+        'medios': ['Redes Sociales', 'Email Marketing', 'Publicidad en línea'],
+        'segmentos_objetivo': ['Todos los compradores'],
+        'personas_alcanzadas': 100000,
+        'tasa_conversion': 0.03
+    },
+    {
+        'nombre': 'Navidad',
+        'presupuesto': 60000,
+        'inicio': '2023-12-01',
+        'fin': '2023-12-24',
+        'medios': ['Publicidad en tiendas físicas', 'Redes Sociales', 'Publicidad impresa'],
+        'segmentos_objetivo': ['Todos los compradores'],
+        'personas_alcanzadas': 120000,
+        'tasa_conversion': 0.04
+    }
+]
+
+# EJERCICIO 5:
+# Calculamos las conversiones de cada campaña multiplicando el número de personas alcanzadas por la tasa de conversión
+for campaña in lista_campañas:
+    campaña["conversiones"] = int(campaña["personas_alcanzadas"] * campaña["tasa_conversion"])
+
+# Imprimimos el nombre de cada campaña junto con el número de conversiones
+for campaña in lista_campañas:
+    print(campaña["nombre"], ":", campaña["conversiones"])
+
+# Output: 
+# Rebajas de Enero : 1000
+# San Valentín : 1500
+# Día del Padre : 750
+# Día de la Madre : 1120
+# Día del Friki : 1000
+# Black Friday : 3000
+# Navidad : 4800
